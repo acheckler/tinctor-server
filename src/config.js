18e12939ad00d65618f6a76f12421414eb20caa2
acheckler <allisonheckler@gmail.com>
2021-07-01 10:20:31 -0500

    heroku cors

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    // CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000/',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000/',
     REACT_APP_DATABASE_URL: process.env.REACT_APP_DATABASE_URL || 'postgresql://allison@localhost/batch',
     REACT_APP_TEST_DATABASE_URL: process.env.REACT_APP_TEST_DATABASE_URL || "postgresql://allison@localhost/batch-test"
 
