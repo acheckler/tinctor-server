e1cd8c6a4849d81e1a504b3c54de03efc9ff61cd
acheckler <allisonheckler@gmail.com>
2021-07-01 13:45:20 -0500

    enabling cors

--- src/app.js
+++ src/app.js
@@ -18,9 +18,10 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
-app.use(cors({
-    origin: CLIENT_ORIGIN
-}))
+app.options('*', cors())
+// app.use(cors({
+//     origin: CLIENT_ORIGIN
+// }))
 
 app.use('/projects', projectsRouter)
 app.use('/ingredients', ingredientsRouter)
